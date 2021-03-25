6c75f03132bed97e171a3a3f04f816f78e7ab789
Phawit Wattanavanichakorn <phawit.w@horganice.in.th>
2021-03-25 11:35:55 +0700

    init

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -11,8 +11,7 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer name="Tab 1 page" @click="$router.push('/tabs2/test/tab1')" />
     </ion-content>
   </ion-page>
 </template>
---
+++ src/views/Tabs2.vue
@@ -0,0 +1,71 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar color="primary">
+        <ion-title>Tabs2</ion-title>
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/tabs"></ion-back-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-tabs>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/tabs2/test/tab1">
+          <ion-icon :icon="triangle" />
+          <ion-label>Tab 1</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab2" href="/tabs2/test/tab2">
+          <ion-icon :icon="ellipse" />
+          <ion-label>Tab 2</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab3" href="/tabs2/test/tab3">
+          <ion-icon :icon="square" />
+          <ion-label>Tab 3</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonBackButton,
+} from "@ionic/vue";
+import { ellipse, square, triangle } from "ionicons/icons";
+
+export default {
+  name: "Tabs",
+  components: {
+    IonLabel,
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonButtons,
+    IonBackButton,
+  },
+  setup() {
+    return {
+      ellipse,
+      square,
+      triangle,
+    };
+  },
+};
+</script>
